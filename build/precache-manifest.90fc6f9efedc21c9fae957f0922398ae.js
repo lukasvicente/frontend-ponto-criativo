dcd9c2a7b2d72169c3f51c334e907ebb1ebab9d8
lucas <lucasvicente2@hotmail.com>
2020-08-17 09:34:14 -0300

    17-08-2020 09:30 - generate build

---
+++ build/precache-manifest.90fc6f9efedc21c9fae957f0922398ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39805d0951facbe5d91453e10cd14b86",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d84daa5d08d3034791c",
+    "url": "/static/css/main.87020218.chunk.css"
+  },
+  {
+    "revision": "7ab0609ff2a690c40ca9",
+    "url": "/static/js/2.d507be2b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.d507be2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d84daa5d08d3034791c",
+    "url": "/static/js/main.ce2caca2.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
